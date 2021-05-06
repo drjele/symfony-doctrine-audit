0875da8eda37f737b2f6443a443567593f8ea8d8
Constantin Jeledintan <constantin.jeledintan@multinode.ro>
2021-05-06 13:29:30 +0300

    remove invalid Copyright

--- src/Annotation/Ignore.php
+++ src/Annotation/Ignore.php
@@ -1,12 +1,5 @@
 <?php
 
-/*
- * Copyright (c) 2018-2020 Multinode Network Srl. All rights reserved.
- * This source file is subject to the LICENSE bundled with this source code part of MNN CoreAPI.
- *
- * (authors) TDD SD Team
- */
-
 declare(strict_types=1);
 
 /*
